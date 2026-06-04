b6cbe83fb1be86f0614bf7519357960d7d097cd4
Hugocrosse <salazarperezh21@gmail.com>
2026-06-03 21:16:26 -0600

    Se hizo la conexion a la base de datos

--- js/index.js
+++ js/index.js
@@ -1,8 +1,14 @@
-document.addEventListener('DOMContentLoaded', function() {
-  // nav menu
-  const menus = document.querySelectorAll('.side-menu');
+btnAgregarPlatillo = document.getElementById('btnAgregarPlatillo')
+
+document.addEventListener('DOMContentLoaded', FUCTION() {
+
+  const menus = document.querySelectorAll ('.side-menu');
   M.Sidenav.init(menus, {edge: 'right'});
-  // add recipe form
-  const forms = document.querySelectorAll('.side-form');
-  M.Sidenav.init(forms, {edge: 'left'});
+
+  const forms = document.querySelectorAll ('.side-menu');
+  M.Sidenav.init(menus, {edge: 'left'});
+});
+
+btnAgregarPlatillo.addEventListener('click', function() {
+  alert('Platillo Agregado');
 });
