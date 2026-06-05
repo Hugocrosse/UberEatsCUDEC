c86e7ed848e6a3ecbc5590c3b9098c7ac574298b
Hugocrosse <salazarperezh21@gmail.com>
2026-06-04 21:25:18 -0600

    Mostramos Platillos en aplicacion

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
-btnAgregarPlatillo = document.getElementById('btnAgregarPlatillo')
-
-document.addEventListener('DOMContentLoaded', FUCTION() {
+let contenido ='';                   
+document.addEventListener('DOMContentLoaded', function() {
 
   const menus = document.querySelectorAll ('.side-menu');
   M.Sidenav.init(menus, {edge: 'right'});
@@ -12,3 +11,26 @@ document.addEventListener('DOMContentLoaded', FUCTION() {
 btnAgregarPlatillo.addEventListener('click', function() {
   alert('Platillo Agregado');
 });
+
+function mostrarPlatillo(platillo, id) {
+  contenido += `
+  <div class="card-panel recipe white row"
+  data-id="${id}">
+    <div class="recipe-details">
+      <div class="recipe-title">
+          ${platillo.nombre}
+      </div>
+      <div class="recipe-ingredients">
+          ${platillo.ingredientes}
+      </div>
+      <div class="recipe-title">
+        Precio: $${platillo.Precio}
+      </div>
+    </div>
+    <div class="recipe-delete">
+      <i class="material-icons" data-id="${id}">delete_outline</i>
+    </div>
+  </div>`;
+  document.querySelector('.recipes').innerHTML = contenido
+ 
+}
